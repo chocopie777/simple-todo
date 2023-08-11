8df686c8cbd7fc6a25d45476b6e3cec0a74b3bcf
chocopie777 <egoran99@gmail.com>
2023-08-11 17:52:01 +0500

    started working on functionality

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,23 @@
-console.log("new branch")
+const task_creation_button = document.getElementById('task-creation-button');
+const input = document.getElementById('input');
+
+function getTasks() {
+    let data = localStorage.getItem('tasks');
+    if (data !== null) {
+        return JSON.parse(data);
+    } else {
+        return false;
+    }
+}
+
+function setTask(text) {
+    let data = getTasks();
+    if (data === false) {
+        data = {};
+    }
+
+}
+
+task_creation_button.addEventListener('click', (event) => {
+    setTask(input.value);
+});
